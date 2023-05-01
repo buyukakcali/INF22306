fe7579f18f650ae095c2a4c422d381cba24b55fd
buyukakcali <buyukakcali@gmail.com>
2023-05-01 15:05:38 +0200

    dosya adı değişikliği 2

--- f4_2_gw_flying_distance5.py
+++ f4_2_gw_flying_distance5.py
@@ -13,23 +13,24 @@ OUTPUTS: the output of this is a text file in Python project folder containing t
 
 def data_cities():
     d = {}
-
     Amsterdam = ('Amsterdam', ('52'+chr(176), '22\'', 'N'), ('4'+chr(176), '32\'', 'E'))
     Montreal = ('Montreal', ('45'+chr(176), '52\'', 'N'), ('73'+chr(176), '35\'', 'W'))
     Auckland = ('Auckland', ('36'+chr(176), '52\'', 'S'), ('174'+chr(176), '45\'', 'E'))
     d['Amsterdam'] = Amsterdam
     d['Montreal'] = Montreal
     d['Auckland'] = Auckland
-
     return d
 
-    # Writing to text file
-    dict1 = data_cities()
-    
-    with open('data_cities.txt', 'w') as file:
-        for city, data in dict1.items():
+# Writing to text file
+def save_to_file(document):
+    city_dict1 = data_cities()
+    with open(document, 'w') as file:
+        for city, data in city_dict1.items():
             name = data[0]
             latitude = f"{data[1][0]} {data[1][1]} {data[1][2]}"
             longitude = f"{data[2][0]} {data[2][1]} {data[2][2]}"
             line = f"Name: {name} Latitude: {latitude} Longitude: {longitude}\n"
             file.write(line)
+
+if __name__ == '__main__':
+    save_to_file('f_4_2_data_cities.txt')
