7c14c350baebbe14199dc64ef7bb4199bd8bde38
buyukakcali <buyukakcali@gmail.com>
2023-05-05 11:35:38 +0200

    code update (continuously)

--- exam2.py
+++ exam2.py
@@ -1,4 +1,3 @@
-
 def list_drink():
     drink = 'randomstring'
     drinks = []
@@ -6,9 +5,11 @@ def list_drink():
     while drink != '':
         drink = input('What drink would you like to have?').lower()
         if drink != '':
-            drinks += [drink] # This is my only mistake (converting the drink variable to an element of a list) after the exam notes..
+            drinks += [drink]  # This is my only mistake (converting the drink variable to an element of a list) after
+            # the exam notes..
             count = len(drinks)
         else:
             return (drinks, count)
+
 
 print(list_drink())
--- f5_3_class_for_polygon.py
+++ f5_3_class_for_polygon.py
@@ -1,18 +1,33 @@
 
 
 class Polygon:
-    """Shape of a Triangle with 3 points, point A at the origin"""
+    points: list
 
-    def constructor(self):   # constructor returns tuples
-        points = []
-        points += [(0, 0)]
-        for i in range(self - 1):
-            points[i+1]= tuple(input('Enter new point, use comma between two values'))
-        return points
+    def __init__(self, points: list):
+        # print('Given points: ', points)
+        self.points = points
+        self.total_circumference = 0
+        self.total_area = 0
 
-    def __init__(self, points:tuple):
-        self.points = self.constructor(points)
+    @property
+    def calculate_circumference(self):
+        for i in range(len(self.points)):   # [[0, 0], [3, 0], [0, 4]] imagine this list while checking the code
+            self.total_circumference += abs(((self.points[i-1][0] - self.points[i][0]) ** 2) + ((self.points[i-1][1] - self.points[i][1]) ** 2)) ** 0.5
+        return self.total_circumference
+
+    def calculate_area(self):
+        total_area = 0
+        for i in range(len(self.points)):   # [[6, 4], [0, 4], [0, 0], [3, 1]] imagine this list while checking the code
+            temp_area = ((self.points[i-1][1] + self.points[i][1]) / 2) * (self.points[i-1][0] - self.points[i][0])
+            total_area += temp_area
+        self.total_area = abs(total_area)
+        return self.total_area
 
 
-poly1 = Polygon
-poly1.constructor(3)
+if __name__ == '__main__':
+    poly = [[6, 4], [0, 4], [0, 0], [3, 1]]     # IMPORTANT: You have to add the points in the correct order
+
+    poly1 = Polygon(poly)
+
+    print(poly1.calculate_circumference)
+    print(poly1.calculate_area())
