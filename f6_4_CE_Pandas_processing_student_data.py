65e2231b9ec23972caeffa786490b79ab569f19d
buyukakcali <buyukakcali@gmail.com>
2023-05-07 23:44:47 +0200

    Merge remote-tracking branch 'origin/master'

--- f6_4_CE_Pandas_processing_student_data.py
+++ f6_4_CE_Pandas_processing_student_data.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-exam_data = {'student_name': ['Daan', 'Emma', 'Katherine', 'Luuk',
-'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Sophie'],
-'grade': [8.5, 9, 6.5, np.nan, 6, 5, 7.5, np.nan, 4, 7],
-'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-'qualify': ['yes', 'yes', 'yes', 'no grade', 'yes', 'no', 'yes', 'no grade', 'no', 'yes']}
+exam_data = dict(
+    student_name=['Daan', 'Emma', 'Katherine', 'Luuk', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Sophie'],
+    grade=[8.5, 9, 6.5, np.nan, 6, 5, 7.5, np.nan, 4, 7],
+    attempts=[1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    qualify=['yes', 'yes', 'yes', 'no grade', 'yes', 'no', 'yes', 'no grade', 'no', 'yes']
+)
 
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-
 
 # 1. Create and display a DataFrame from a specified dictionary data which has the index labels.
 
--- f6_6_Pandas_flying_distances9.py
+++ f6_6_Pandas_flying_distances9.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import f4_2_gw_flying_distance5 as fd5
 
+
 def rad_to_deg(rad):
     dir = 1
-    if rad<0:
+    if rad < 0:
         dir = -1
     tot_min = abs(rad) * 60
-    return  [(tot_min/60).__trunc__(), (tot_min%60).__trunc__(), dir]
+    return [(tot_min / 60).__trunc__(), (tot_min % 60).__trunc__(), dir]
 
-def pd_to_dict(file:str):
+
+def pd_to_dict(file: str):
     dict1 = {}
     df = pd.read_json(file)
     list1 = df.values.tolist()
@@ -16,6 +18,7 @@ def pd_to_dict(file:str):
     for i in range(len(list1)):
         dict1[list1[i][0]] = (list1[i][0], rad_to_deg(list1[i][1]), rad_to_deg(list1[i][2]))
     return dict1
+
 
 if __name__ == '__main__':
     dictionary = pd_to_dict('ch.json')
