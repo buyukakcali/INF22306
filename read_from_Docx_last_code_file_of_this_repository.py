7f48b888fa39ff93d8238d2126f5dfaed61c74f7
buyukakcali <buyukakcali@gmail.com>
2023-07-17 01:09:00 +0200

    Merge remote-tracking branch 'origin/master'

--- read_from_Docx_last_code_file_of_this_repository.py
+++ read_from_Docx_last_code_file_of_this_repository.py
@@ -1,5 +1,6 @@
 import docx
 
+
 def read_docx(file_path):
     document = docx.Document(file_path)
     text = []
@@ -9,10 +10,12 @@ def read_docx(file_path):
         for i in text:
             j += i.strip('\n')
     print(j)
-    #return 'n'.join(text)
-    with open(output_file, 'w') as ex1output: # finally, write the result list to the file (be careful, 'writelines()' is used)
+    # return 'n'.join(text)
+    with open(output_file, 'w') as ex1output:
+        # finally, write the result list to the file (be careful, 'writelines()' is used)
         ex1output.writelines(j)
     return 'Check the output file'  # this is for information. actually it isn't necessary. We can return "True" or "1"
+
 
 if __name__ == '__main__':
     output_file = 'C:\\Users\\user\\Desktop\\QUIZ4.txt'
